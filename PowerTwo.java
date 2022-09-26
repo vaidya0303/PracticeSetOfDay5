4693bea2f0baae8d4e182f9bec61c579bb6b1e8e
vaidya0303 <vaidya0304@gmail.com>
2022-09-26 20:14:36 +0530

    number with power of two

---
+++ PowerTwo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PowerTwo{
+
+
+	void multiply(int n){
+		int num = 1;
+		for(int i=0; i<n; i++) {
+			num = num*2;
+			System.out.println(num);
+		}
+	}
+		public static void main(String[] args){
+			PowerTwo P2 = new PowerTwo();
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter a number");
+			int n = sc.nextInt();
+			P2.multiply(n);
+		}
+	}
